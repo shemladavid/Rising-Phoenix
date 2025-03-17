da984a923a74ed5105a46f882c5f19e1d4426538
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-03-17 20:17:19 +0200

    SDG

--- script/c100000721.lua
+++ script/c100000721.lua
@@ -1,23 +1,24 @@
 --scripted and created by rising phoenix
-function c100000721.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	Duel.EnableGlobalFlag(GLOBALFLAG_SELF_TOGRAVE)
 		--act in hand
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE)
 	e7:SetCode(EFFECT_TRAP_ACT_IN_HAND)
-	e7:SetCondition(c100000721.handcon)
+	e7:SetCondition(s.handcon)
 	c:RegisterEffect(e7)
 		local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetOperation(c100000721.aclimit1)
+	e2:SetOperation(s.aclimit1)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e3:SetCode(EVENT_CHAIN_NEGATED)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetOperation(c100000721.aclimit2)
+	e3:SetOperation(s.aclimit2)
 	c:RegisterEffect(e3)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
@@ -25,8 +26,8 @@ function c100000721.initial_effect(c)
 	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetTargetRange(0,1)
-	e4:SetCondition(c100000721.econ)
-	e4:SetValue(c100000721.elimit)
+	e4:SetCondition(s.econ)
+	e4:SetValue(s.elimit)
 	c:RegisterEffect(e4)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -39,7 +40,7 @@ function c100000721.initial_effect(c)
 	e12:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e12:SetRange(LOCATION_SZONE)
 	e12:SetCode(EFFECT_SELF_TOGRAVE)
-	e12:SetCondition(c100000721.descon)
+	e12:SetCondition(s.descon)
 	c:RegisterEffect(e12)
 	--prevent set
 	local e13=Effect.CreateEffect(c)
@@ -47,57 +48,57 @@ function c100000721.initial_effect(c)
 	e13:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e13:SetCode(EFFECT_CANNOT_SSET)
 	e13:SetRange(LOCATION_SZONE)
-	e13:SetCondition(c100000721.setcon1)
-	e13:SetTarget(c100000721.settg)
+	e13:SetCondition(s.setcon1)
+	e13:SetTarget(s.settg)
 	e13:SetTargetRange(0,1)
 	c:RegisterEffect(e13)
 		--Check for single Set
-	if not c100000721.global_check then
-		c100000721.global_check=true
+	if not s.global_check then
+		s.global_check=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_SSET)
-		ge1:SetOperation(c100000721.checkop)
+		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
 end
 end
-function c100000721.checkop(e,tp,eg,ep,ev,re,r,rp)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	if eg:IsExists(Card.IsPreviousLocation,1,nil,LOCATION_HAND) then
-		Duel.RegisterFlagEffect(rp,100000721,RESET_PHASE+PHASE_END,0,1)
+		Duel.RegisterFlagEffect(rp,id,RESET_PHASE+PHASE_END,0,1)
 	end
 end
-function c100000721.settg(e,c)
+function s.settg(e,c)
 	return c:IsLocation(LOCATION_HAND)
 end
-function c100000721.setcon1(e)
-	return Duel.GetFlagEffect(1-e:GetHandlerPlayer(),100000721)>0
+function s.setcon1(e)
+	return Duel.GetFlagEffect(1-e:GetHandlerPlayer(),id)>0
 end
-function c100000721.descon(e)
-	return not Duel.IsExistingMatchingCard(c100000721.filterd,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+function s.descon(e)
+	return not Duel.IsExistingMatchingCard(s.filterd,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
 end
-function c100000721.ccfilter(c)
+function s.ccfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x764) and c:IsType(TYPE_MONSTER)
 end
-function c100000721.handcon(e)
-	return Duel.IsExistingMatchingCard(c100000721.filter,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+function s.handcon(e)
+	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
 end
-function c100000721.filter(c)
+function s.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x764) and c:IsType(TYPE_MONSTER)
 end
-function c100000721.filterd(c)
+function s.filterd(c)
 	return c:IsFaceup() and c:IsSetCard(0x764) and c:IsType(TYPE_MONSTER)
 end
-function c100000721.aclimit1(e,tp,eg,ep,ev,re,r,rp)
+function s.aclimit1(e,tp,eg,ep,ev,re,r,rp)
 	if ep==tp or not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
-	e:GetHandler():RegisterFlagEffect(100000721,RESET_EVENT+0x3ff0000+RESET_PHASE+PHASE_END,0,1)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+0x3ff0000+RESET_PHASE+PHASE_END,0,1)
 end
-function c100000721.aclimit2(e,tp,eg,ep,ev,re,r,rp)
+function s.aclimit2(e,tp,eg,ep,ev,re,r,rp)
 	if ep==tp or not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
-	e:GetHandler():ResetFlagEffect(100000721)
+	e:GetHandler():ResetFlagEffect(id)
 end
-function c100000721.econ(e)
-	return e:GetHandler():GetFlagEffect(100000721)~=0
+function s.econ(e)
+	return e:GetHandler():GetFlagEffect(id)~=0
 end
-function c100000721.elimit(e,te,tp)
+function s.elimit(e,te,tp)
 	return te:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
