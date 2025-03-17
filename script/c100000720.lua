4895e4e5aced9367d4c2f6408aff6a51a15f5671
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-03-17 20:24:55 +0200

    Update c100000720.lua

--- script/c100000720.lua
+++ script/c100000720.lua
@@ -2,30 +2,24 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	Duel.EnableGlobalFlag(GLOBALFLAG_SELF_TOGRAVE)
-		--act in hand
+	--act in hand
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE)
 	e7:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	e7:SetCondition(s.handcon)
 	c:RegisterEffect(e7)
-		--activate limit
-	local e11=Effect.CreateEffect(c)
-	e11:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e11:SetCode(EVENT_CHAINING)
-	e11:SetRange(LOCATION_SZONE)
-	e11:SetOperation(s.aclimit1)
-	c:RegisterEffect(e11)
+	--activate limit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EVENT_CHAIN_NEGATED)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetOperation(s.aclimit2)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.aclimit1)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e3:SetRange(LOCATION_SZONE)
+	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
 	e3:SetCondition(s.econ1)
 	e3:SetValue(s.elimit)
@@ -58,15 +52,11 @@ function s.filter(c)
 end
 function s.aclimit1(e,tp,eg,ep,ev,re,r,rp)
 	if ep==tp or not re:IsActiveType(TYPE_MONSTER) then return end
-	e:GetHandler():RegisterFlagEffect(100000720,RESET_EVENT+0x3ff0000+RESET_PHASE+PHASE_END,0,1)
-end
-function s.aclimit2(e,tp,eg,ep,ev,re,r,rp)
-	if ep==tp or not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
-	e:GetHandler():ResetFlagEffect(100000720)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_CONTROL+RESET_PHASE+PHASE_END,0,1)
 end
 function s.econ1(e)
-	return e:GetHandler():GetFlagEffect(100000720)~=0
+	return e:GetHandler():GetFlagEffect(id+1)~=0
 end
 function s.elimit(e,re,tp)
-	return re:IsActiveType(TYPE_MONSTER) and not re:GetHandler():IsImmuneToEffect(e)
+	return re:IsActiveType(TYPE_MONSTER)
 end
