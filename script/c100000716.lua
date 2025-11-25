add4b07fad4f5798599ddf54cbb5d94339ad000f
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-11-25 21:38:27 +0200

    dsfguhifhdstrah

--- script/c100000716.lua
+++ script/c100000716.lua
@@ -33,6 +33,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	--tohand
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_TO_HAND)
